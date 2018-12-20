777f13fec8e1800df112a48c7fbfeb7e7ab68d96
TwinterT <1012179010@qq.com>
2018-12-20 13:44:07 +0800

    E7 repair bugs

--- SimpleController/src/sc/ustc/utils/ProxyUserBeanHandler.java
+++ SimpleController/src/sc/ustc/utils/ProxyUserBeanHandler.java
@@ -17,38 +17,42 @@ public class ProxyUserBeanHandler implements MethodInterceptor {
 	public ProxyUserBeanHandler(Map<String, Property> map) {
 		lazyProperties = map;
 	}
-	
-	
+
 	@Override
 	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
 		System.out.println("check for lazy loading!!!");
-		if(method.getName().startsWith("get") && proxy.invokeSuper(obj, args) == null) {
-			//当调用的是get方法并且获得的对象是null时，启动懒加载
-			
+		if (method.getName().startsWith("get") && proxy.invokeSuper(obj, args) == null) {
+			// 当调用的是get方法并且获得的对象是null时，启动懒加载
+
 			String fieldName = method.getName().substring(3);
-			fieldName = fieldName.substring(0, 1).toLowerCase()+fieldName.substring(1);
-			
-			System.out.println("filedName:"+fieldName);
+			fieldName = fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1);
+
+			System.out.println("filedName:" + fieldName);
 			Property property = lazyProperties.get(fieldName);
-			if(property !=null) {
-				//查询获得结果集合
+			if (property != null) {
+				// 查询获得结果集合
 				ResultSet resultSet = Conversation.query((BaseBean) obj);
-				
-				//查找并且调用set方法
-				String setMethodName = "set" +method.getName().substring(3); 
-				System.out.println("setMethodName:"+setMethodName);
-				Method[] methods = obj.getClass().getDeclaredMethods();
-				for(Method proxyMethod:methods) {
-					System.out.println("method name is :"+proxyMethod.getName());
-					if(proxyMethod.getName().equals(setMethodName)) {
-						proxyMethod.invoke(obj, resultSet.getString(property.getColumn()));
+				if (resultSet != null) {
+
+					String result = resultSet.getString(property.getColumn());
+					if (result != null) {
+
+						// 查找并且调用set方法
+						String setMethodName = "set" + method.getName().substring(3);
+						System.out.println("setMethodName:" + setMethodName);
+						Method[] methods = obj.getClass().getDeclaredMethods();
+						for (Method proxyMethod : methods) {
+							if (proxyMethod.getName().equals(setMethodName)) {
+								proxyMethod.invoke(obj, result);
+							}
+						}
 					}
-				} 
-				
+				}
+
 			}
 		}
 
-		//返回方法的执行结果
+		// 返回方法的执行结果
 		return proxy.invokeSuper(obj, args);
 	}
 }
